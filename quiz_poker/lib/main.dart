58d8a6a609475238e1aa7e506dc5731b7f1faa1f
Vlad <vladislavfridkin@gmail.com>
2022-12-24 08:23:15 +1100

    task f4

--- quiz_poker/lib/main.dart
+++ quiz_poker/lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
+const String imgSource =
+    'https://st.depositphotos.com/2654883/3574/v/950/depositphotos_35743665-stock-illustration-hip-hop-latter-symbol-of.jpg';
+const String category = 'Hip Hop';
+const String question = 'What was the first rap to hit the Billboard?';
+
+const List<String> questions = [
+  'What was the first rap to hit the Billboard?',
+  'Run-D.M.C. collaborated with rock musicians Steven Tyler and Joe Perry, of Aerosmith, on a rap remake of which of their band’s songs?',
+  'The original "four elements" of hip-hop culture consist of rapping, deejaying, break dancing (or "B-boying"), and what else?',
+  'Ginuwine’s "Pony," Jay-Z’s "Big Pimpin’," Missy Elliott’s "Work It," and Nelly Furtado’s "Promiscuous" were all produced by whom?',
+  'The title of the worldwide hit "Gangnam Style" (2012) refers to a neighborhood in which city?',
+  'Sir Mix-A-Lot’s 1992 hit "Baby Got Back" was sampled in a 2014 song by which female rapper?',
+  'Which singer has been called the Queen of Hip-Hop Soul?',
+  'What was the first rap song to hit number one on the Billboard Hot 100 chart?',
+  'Chuck D of Public Enemy famously referred to rap as "the black" what?',
+  'What is the birth name of the rapper and actor familiarly known as Ice Cube?',
+];
+
 void main() {
   runApp(const MyApp());
 }
@@ -24,33 +42,45 @@ class ScaffoldPage extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.green,
       ),
-      body: const QuestionItem(),
+      body: const QuestionItemList(),
       floatingActionButton: const Fab(),
     );
   }
 }
 
-class QuestionItem extends StatelessWidget {
-  const QuestionItem({super.key});
+class QuestionItemList extends StatelessWidget {
+  const QuestionItemList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Card(
-          color: Colors.green.shade100,
-          child: const ListTile(
-            leading: Icon(
-              Icons.help,
-              size: 40,
-              color: Colors.green,
-            ),
-            title: Text('Category: Flutter self awareness'),
-            subtitle: Text('What is this listTile doing here all alone?'),
+    return SingleChildScrollView(
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: questions
+              .map((question) => QuestionItem(questionText: question))
+              .toList()),
+    );
+  }
+}
+
+class QuestionItem extends StatelessWidget {
+  final String questionText;
+  const QuestionItem({super.key, required this.questionText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.green.shade100,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading: const CircleAvatar(
+            backgroundImage: NetworkImage(imgSource),
           ),
+          title: const Text(category),
+          subtitle: Text(questionText),
         ),
-      ],
+      ),
     );
   }
 }
