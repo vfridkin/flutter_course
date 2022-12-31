9e63c6c37bd2ed51a59194246cdf0fe480fc5deb
Vlad <vladislavfridkin@gmail.com>
2022-12-31 15:55:56 +1100

    Trying f7 with CSV

--- quiz_poker/lib/main.dart
+++ quiz_poker/lib/main.dart
@@ -1,39 +1,21 @@
+// Use a List View Builder
+// • Use a ListView to display your questions
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:csv/csv.dart';
 import 'package:flutter/material.dart';
 
 final kBackColour = Colors.green[50];
 const kMainColour = Colors.green;
 const kSnackColour = Colors.red;
 
-const String imgAsset = 'assets/images/hip-hop.webp';
-const String imgNetwork =
+const String kImgAsset = 'assets/images/hip-hop.webp';
+const String kImgSource =
     'https://st.depositphotos.com/2654883/3574/v/950/depositphotos_35743665-stock-illustration-hip-hop-latter-symbol-of.jpg';
-const String category = 'Hip Hop';
-const List<String> questions = [
-  'What was the first rap to hit the Billboard?',
-  'Run-D.M.C. collaborated with rock musicians Steven Tyler and Joe Perry, of Aerosmith, on a rap remake of which of their band’s songs?',
-  'The original "four elements" of hip-hop culture consist of rapping, deejaying, break dancing (or "B-boying"), and what else?',
-  'Ginuwine’s "Pony," Jay-Z’s "Big Pimpin’," Missy Elliott’s "Work It," and Nelly Furtado’s "Promiscuous" were all produced by whom?',
-  'The title of the worldwide hit "Gangnam Style" (2012) refers to a neighborhood in which city?',
-  'Sir Mix-A-Lot’s 1992 hit "Baby Got Back" was sampled in a 2014 song by which female rapper?',
-  'Which singer has been called the Queen of Hip-Hop Soul?',
-  'What was the first rap song to hit number one on the Billboard Hot 100 chart?',
-  'Chuck D of Public Enemy famously referred to rap as "the black" what?',
-  'What is the birth name of the rapper and actor familiarly known as Ice Cube?',
-  'In which U.S city was the hip hop culture created?',
-  'Which actor and rapper is often known as Ice Cube?',
-  'The American hip hop star Dr. Dre also has a popular brand of which product with his name?',
-  'What is the name of the first album by Jay-Z?',
-  'What was the first rap name of Biggie Smalls, also known as The Notorious B.I.G?',
-  'Run-D.M.C worked with rock musicians Joe Perry and Steven Tyler to remake which rap song of their band?',
-  'Which of the following people was not a member of the hip hop group Fugees?',
-  'Which of the following is a magazine that focuses on the world of hip hop?',
-  'Chuck D, the leader of the rap group Public Enemy, once called rap “the black …”',
-  'The famous rapper Karim Kharbouch, often known as French Montana was born in which country?',
-];
-final GlobalKey<_QuestionItemState> _childKey = GlobalKey();
-void _nextQuestion() {
-  _childKey.currentState?.nextQuestion();
-}
+const String kDataAsset = 'assets/data/quiz.csv';
+const String kCategory = 'Hip Hop';
 
 void main() {
   runApp(const MyApp());
@@ -53,82 +35,91 @@ class ScaffoldPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Create list of question indices that are shuffled to get random non repeating questions
-    final List<int> questionIndices = [
-      for (var i = 0; i < questions.length; i++) i
-    ];
-    questionIndices.shuffle();
-
-    QuestionItem questionItem = QuestionItem(
-      key: _childKey,
-      indices: questionIndices,
-    );
-
     return Scaffold(
-      body: questionItem,
-      appBar: AppBar(
-        title: const Text('Quiz Poker'),
-        centerTitle: true,
-        backgroundColor: kMainColour,
-      ),
-      floatingActionButton: const FloatingActionButton(
-        backgroundColor: kMainColour,
-        onPressed: _nextQuestion,
-        child: Icon(
-          Icons.navigate_next_outlined,
+        appBar: AppBar(
+          title: const Text('Quiz Poker'),
+          centerTitle: true,
+          backgroundColor: kMainColour,
         ),
-      ),
-    );
+        body: const QuestionItemList());
   }
 }
 
-class QuestionItem extends StatefulWidget {
-  final List<int> indices;
-  const QuestionItem({super.key, required this.indices});
+class QuestionItemList extends StatefulWidget {
+  const QuestionItemList({super.key});
 
   @override
-  State<QuestionItem> createState() => _QuestionItemState();
+  State<QuestionItemList> createState() => _QuestionItemListState();
 }
 
-class _QuestionItemState extends State<QuestionItem> {
-  int index = 0;
-  late int questionIndex;
-  late String question;
-
-  @override
-  void initState() {
-    super.initState();
-    questionIndex = widget.indices[index];
-    question = questions[questionIndex];
-  }
-
-  void nextQuestion() {
-    setState(() {
-      index++;
-      questionIndex = widget.indices[index];
-      question = questions[questionIndex];
-    });
-  }
+class _QuestionItemListState extends State<QuestionItemList> {
+  DataService dataService = DataService();
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return FutureBuilder<List<List<dynamic>>>(
+      future: dataService.getCsvDataFromLocal(),
+      builder: (context, AsyncSnapshot<List<List<dynamic>>> snapshot) {
+        if (snapshot.hasData) {
+          return LayoutBuilder(
+              builder: (BuildContext context, BoxConstraints constraints) {
+            final maxTiles = constraints.maxWidth ~/ 300;
+            final tileWidth = constraints.maxWidth / maxTiles;
+
+            List<List<dynamic>>? data = snapshot.data;
+            List<dynamic> questions = data!.map((row) => row[0]).toList();
+
+            return ListView.separated(
+              itemCount: questions.length,
+              scrollDirection:
+                  constraints.maxWidth > 600 ? Axis.horizontal : Axis.vertical,
+              separatorBuilder: (context, index) => constraints.maxWidth > 600
+                  ? const SizedBox(width: 10)
+                  : const SizedBox(height: 10),
+              itemBuilder: (context, index) => QuestionItem(
+                questionText: questions[index],
+                tileWidth: tileWidth,
+              ),
+            );
+          });
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
+
+class QuestionItem extends StatelessWidget {
+  final String questionText;
+  final double tileWidth;
+
+  const QuestionItem(
+      {super.key, required this.questionText, required this.tileWidth});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: tileWidth,
+      decoration: BoxDecoration(
+          color: kBackColour, borderRadius: BorderRadius.circular(10)),
       child: ListTile(
-        tileColor: kBackColour,
-        contentPadding: const EdgeInsets.all(8.0),
-        title: const Text(category),
-        subtitle: Text(question),
         leading: const CircleAvatar(
-          radius: 50,
-          backgroundImage: NetworkImage(imgNetwork),
-          // backgroundImage: AssetImage(imgAsset),
-          // child: ClipRRect(
-          //   borderRadius: BorderRadius.circular(45),
-          //   child: Image.asset(imgAsset),
-          // ),
+          backgroundImage: NetworkImage(kImgSource),
         ),
+        title: const Text(kCategory),
+        subtitle: Text(questionText),
       ),
     );
+  }
+}
+
+class DataService {
+  Future<List<List>> getCsvDataFromLocal() async {
+    final csvFile = File(kDataAsset).openRead();
+    return await csvFile
+        .transform(utf8.decoder)
+        .transform(const CsvToListConverter())
+        .toList();
   }
 }
--- quiz_poker/lib/task_f7.dart
+++ quiz_poker/lib/task_f7.dart
@@ -1,109 +1,113 @@
-// Use a List View Builder
-// • Use a ListView to display your questions
+// // Use a List View Builder
+// // • Use a ListView to display your questions
 
-import 'package:csv/csv.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:csv/csv.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 
-final kBackColour = Colors.green[50];
-const kMainColour = Colors.green;
-const kSnackColour = Colors.red;
+// final kBackColour = Colors.green[50];
+// const kMainColour = Colors.green;
+// const kSnackColour = Colors.red;
 
-const String imgAsset = 'assets/images/hip-hop.webp';
-const String imgSource =
-    'https://st.depositphotos.com/2654883/3574/v/950/depositphotos_35743665-stock-illustration-hip-hop-latter-symbol-of.jpg';
-const String dataAsset = 'assets/data/quiz.csv';
-const String category = 'Hip Hop';
-List<List<String>> data = [];
-List<String> questions = [];
+// const String imgAsset = 'assets/images/hip-hop.webp';
+// const String imgSource =
+//     'https://st.depositphotos.com/2654883/3574/v/950/depositphotos_35743665-stock-illustration-hip-hop-latter-symbol-of.jpg';
+// const String dataAsset = 'assets/data/quiz.csv';
+// const String category = 'Hip Hop';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(home: ScaffoldPage());
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(home: ScaffoldPage());
+//   }
+// }
 
-class ScaffoldPage extends StatelessWidget {
-  const ScaffoldPage({super.key});
+// class ScaffoldPage extends StatelessWidget {
+//   const ScaffoldPage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Quiz Poker'),
-          centerTitle: true,
-          backgroundColor: kMainColour,
-        ),
-        body: const QuestionItemList());
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Quiz Poker'),
+//           centerTitle: true,
+//           backgroundColor: kMainColour,
+//         ),
+//         body: const QuestionItemList());
+//   }
+// }
 
-class QuestionItemList extends StatefulWidget {
-  const QuestionItemList({super.key});
+// class QuestionItemList extends StatefulWidget {
+//   const QuestionItemList({super.key});
 
-  @override
-  State<QuestionItemList> createState() => _QuestionItemListState();
-}
+//   @override
+//   State<QuestionItemList> createState() => _QuestionItemListState();
+// }
 
-class _QuestionItemListState extends State<QuestionItemList> {
-  loadAsset() async {
-    var csvFile = await rootBundle.loadString(dataAsset);
-    List<List<String>> csvData = const CsvToListConverter().convert(csvFile);
-    setState(() {
-      data = csvData;
-    });
-  }
+// class _QuestionItemListState extends State<QuestionItemList> {
+//   List<String> questions = [];
 
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-      final maxTiles = constraints.maxWidth ~/ 300;
-      final tileWidth = constraints.maxWidth / maxTiles;
+//   DataService dataService = DataService();
 
-      return ListView.separated(
-        itemCount: questions.length,
-        scrollDirection:
-            constraints.maxWidth > 600 ? Axis.horizontal : Axis.vertical,
-        separatorBuilder: (context, index) => constraints.maxWidth > 600
-            ? const SizedBox(width: 10)
-            : const SizedBox(height: 10),
-        itemBuilder: (context, index) => QuestionItem(
-          questionText: questions[index],
-          tileWidth: tileWidth,
-        ),
-      );
-    });
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder<List<List<String>>>(
+//       future: dataService.getCsvDataFromLocal(),
+//       child: LayoutBuilder(
+//           builder: (BuildContext context, BoxConstraints constraints) {
+//         final maxTiles = constraints.maxWidth ~/ 300;
+//         final tileWidth = constraints.maxWidth / maxTiles;
 
-class QuestionItem extends StatelessWidget {
-  final String questionText;
-  final double tileWidth;
+//         return ListView.separated(
+//           itemCount: questions.length,
+//           scrollDirection:
+//               constraints.maxWidth > 600 ? Axis.horizontal : Axis.vertical,
+//           separatorBuilder: (context, index) => constraints.maxWidth > 600
+//               ? const SizedBox(width: 10)
+//               : const SizedBox(height: 10),
+//           itemBuilder: (context, index) => QuestionItem(
+//             questionText: questions[index],
+//             tileWidth: tileWidth,
+//           ),
+//         );
+//       }),
+//     );
+//   }
+// }
 
-  const QuestionItem(
-      {super.key, required this.questionText, required this.tileWidth});
+// class QuestionItem extends StatelessWidget {
+//   final String questionText;
+//   final double tileWidth;
 
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: tileWidth,
-      decoration: BoxDecoration(
-          color: kBackColour, borderRadius: BorderRadius.circular(10)),
-      child: ListTile(
-        leading: const CircleAvatar(
-          backgroundImage: NetworkImage(imgSource),
-        ),
-        title: const Text(category),
-        subtitle: Text(questionText),
-      ),
-    );
-  }
-}
+//   const QuestionItem(
+//       {super.key, required this.questionText, required this.tileWidth});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       width: tileWidth,
+//       decoration: BoxDecoration(
+//           color: kBackColour, borderRadius: BorderRadius.circular(10)),
+//       child: ListTile(
+//         leading: const CircleAvatar(
+//           backgroundImage: NetworkImage(imgSource),
+//         ),
+//         title: const Text(category),
+//         subtitle: Text(questionText),
+//       ),
+//     );
+//   }
+// }
+
+// class DataService {
+//   Future<List<List<String>>> getCsvDataFromLocal() async {
+//     var csvFile = await rootBundle.loadString(dataAsset);
+//     return const CsvToListConverter().convert(csvFile);
+//   }
+// }
